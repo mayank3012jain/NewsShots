7784df6a47bf7cbbae26387d780b5b2376d23524
mayank_ubuntu <mayank3012jain@gmail.com>
2019-06-27 09:54:00 +0530

    add settings page

---
+++ app/src/main/java/com/oldmonk/newsshots/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.oldmonk.newsshots;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fl_settings, new SettingsFragment())
+                .commit();
+    }
+}
---
+++ app/src/main/java/com/oldmonk/newsshots/SettingsFragment.java
@@ -0,0 +1,24 @@
+package com.oldmonk.newsshots;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        setPreferencesFromResource(R.xml.preferences, s);
+    }
+
+
+}
